0fa96381bd190d40339cffd53737d83fb3111d6a
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2021-12-27 23:57:16 -0300

    Challenge completed

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,13 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: lengthList(list, 0)
+
+  defp lengthList([], acc) do
+    acc
+  end
+
+  defp lengthList(list, acc) do
+    acc = acc + 1
+
+    lengthList(tl(list), acc)
   end
 end
